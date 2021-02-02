1510ded14529db3f321d96d0a5c6fdf4d710ba50
Michael Haberler <git@mah.priv.at>
2021-02-02 17:38:30 +0100

    fix summary update

--- readascent.py
+++ readascent.py
@@ -8,25 +8,46 @@ import traceback
 import sys
 from eccodes import *
 import argparse
-import json
+import orjson
 import geojson
 import logging
 import datetime
 import os
-import metpy.calc
-from metpy.units import units
-from metpy.constants import earth_gravity
 from math import atan, sin, cos
 import brotli
 import pathlib
 import zipfile
 import tempfile
-import geohash2
+import ciso8601
+from pprint import pprint
+from operator import itemgetter
+
+earth_avg_radius =  6371008.7714
+earth_gravity =  9.80665
+
+# metpy is terminally slow, so roll our own sans dimension checking
+def geopotential_height_to_height(gph):
+    geopotential = gph * earth_gravity
+    return (geopotential * earth_avg_radius) / (earth_gravity * earth_avg_radius - geopotential)
+
+
+class OneLineExceptionFormatter(logging.Formatter):
+    def formatException(self, exc_info):
+        result = super(OneLineExceptionFormatter, self).formatException(exc_info)
+        return repr(result) # or format into one line however you want to
+
+    def format(self, record):
+        s = super(OneLineExceptionFormatter, self).format(record)
+        if record.exc_text:
+            s = s.replace('\n', '') + '|'
+        return s
 
 
 def bufr_decode(f, args):
 
     ibufr = codes_bufr_new_from_file(f)
+    if not ibufr:
+        raise Exception("empty file")
     codes_set(ibufr, 'unpack', 1)
 
     missingHdrKeys = 0
@@ -155,15 +176,15 @@ def winds_to_UV(windSpeed, windDirection):
 
 
 def gen_id(h):
+    bn = h.get('blockNumber', 2147483647)
+    sn = h.get('stationNumber', 2147483647)
 
     if 'shipOrMobileLandStationIdentifier' in h:
-        return h['shipOrMobileLandStationIdentifier']
-    elif ('blockNumber' in h and 'stationNumber' in h and
-          h['blockNumber'] != 2147483647 and h['stationNumber'] != 2147483647):
-        return str(h['blockNumber']) + str(h['stationNumber'])
+        return ("mobile", h['shipOrMobileLandStationIdentifier'])
+    elif (bn != 2147483647 and sn != 2147483647):
+        return ("wmo", f'{bn:02d}{sn:03d}')
     else:
-        return geohash2.encode(h['latitude'], h['longitude'])
-
+        return ("location", f"{h['latitude']:.3f}:{h['longitude']:.3f}")
 
 def convert_to_geojson(args, h, samples):
     takeoff = datetime.datetime(year=h['year'],
@@ -173,28 +194,35 @@ def convert_to_geojson(args, h, samples):
                                 hour=h['hour'],
                                 second=h['second'],
                                 tzinfo=None)
-    # if {"foo", "bar"} <= myDict.keys()
+
+    typ, id = gen_id(h)
+
+    ts = ciso8601.parse_datetime(h['typicalDate'] + " " +  h['typicalTime']).timestamp()
 
     properties = {
-        "station_id":  gen_id(h),
+        "station_id":  id,
+        "id_type":  typ,
         "syn_date": h['typicalDate'],
         "syn_time": h['typicalTime'],
-        "firstSeen": takeoff.isoformat(),
+        "syn_timestamp": ts,
+        "firstSeen": takeoff.timestamp(),
         "lat": h['latitude'],
         "lon": h['longitude'],
         "elevation": h['height'],  # ?
         "station_elevation": h['heightOfStationGroundAboveMeanSeaLevel'],  # ?
         "baro_elevation": h['heightOfBarometerAboveMeanSeaLevel'],  # ?
-        "sonde_serial": h['radiosondeSerialNumber'],
-        "sonde_frequency":  h['radiosondeOperatingFrequency'],
+#        "sonde_serial": h['radiosondeSerialNumber'],
+#        "sonde_frequency":  h['radiosondeOperatingFrequency'],
         "sonde_type":  h['radiosondeType']
     }
+    if 'radiosondeSerialNumber' in h:
+        properties['sonde_serial'] = h['radiosondeSerialNumber']
+
+    if 'radiosondeOperatingFrequency' in h:
+        properties['sonde_frequency'] = h['radiosondeOperatingFrequency']
+
     fc = geojson.FeatureCollection([])
     fc.properties = properties
-
-    # add name "Location time" or geocode location
-    # add station_id
-
     lat_t = fc.properties['lat']
     lon_t = fc.properties['lon']
     firstSeen = fc.properties['firstSeen']
@@ -208,13 +236,10 @@ def convert_to_geojson(args, h, samples):
         delta = datetime.timedelta(seconds=s['timePeriod'])
         sampleTime = takeoff + delta
 
-        gph = units.Quantity(gpheight, 'meter')
-        geopot = gph * earth_gravity
-        height = metpy.calc.geopotential_to_height(geopot)
-
-        if height.magnitude < previous_elevation + args.hstep:
+        height = geopotential_height_to_height(gpheight)
+        if height < previous_elevation + args.hstep:
             continue
-        previous_elevation = height.magnitude
+        previous_elevation = height
 
         u, v = winds_to_UV(s['windSpeed'], s['windDirection'])
         if args.winds_dir_speed:
@@ -235,10 +260,10 @@ def convert_to_geojson(args, h, samples):
             "dewpoint": s['dewpointTemperature'],
             "pressure": s['pressure'],
         }
-        f = geojson.Feature(geometry=geojson.Point((lon, lat, height.magnitude)),
+        f = geojson.Feature(geometry=geojson.Point((lon, lat, height)),
                             properties={**properties, **winds})
         fc.features.append(f)
-    fc.properties['lastSeen'] = sampleTime.isoformat()
+    fc.properties['lastSeen'] = sampleTime.timestamp()
     return fc
 
 
@@ -246,21 +271,38 @@ def gen_czml(fc):
     logging.warning(f"not implemented yet")
 
 
-updated_stations = dict()
+updated_stations = []
 
 def gen_output(args, h, s, fn, zip):
     h['samples'] = s
     if args.orig:
         print(h)
 
+    if len(s) < 10:
+        logging.info(f'QC: skipping {fn} from {zip} - only {len(s)} samples')
+        return
+
+    # QC here!
+    if not ({'year', 'month', 'day', 'minute', 'hour', 'second'} <= h.keys()):
+        logging.info(f'QC: skipping {fn} from {zip}')
+        return
+    if h['year'] == 2147483647:
+        logging.info(f'QC: skipping {fn} from {zip}, no date')
+        return
+
+    if  h['second'] > 59:
+        h['second'] = 0
+
+
     fc = convert_to_geojson(args, h, s)
     fc.properties['origin_fn'] = fn
-    fc.properties['origin_zip'] = zip
+    if zip:
+        fc.properties['origin_zip'] = zip
     station_id = fc.properties['station_id']
     logging.debug(
         f'output samples retained: {len(fc.features)}, station id={station_id}')
 
-    updated_stations[station_id] = fc.properties
+    updated_stations.append((station_id, fc.properties))
 
     cext = ""
     if args.brotli:
@@ -272,8 +314,10 @@ def gen_output(args, h, s, fn, zip):
 
     if args.by_basename:
         dest = f'{args.destdir}/{fn}.geojson{cext}'
+        ref = f'{fn}.geojson{cext}'
     else:
         dest = f'{args.destdir}/{cc}/{station_id}_{day}_{time}.geojson{cext}'
+        ref = f'{cc}/{station_id}_{day}_{time}.geojson{cext}'
 
     if args.mkdirs:
         path = pathlib.Path(dest).parent.absolute()
@@ -288,6 +332,7 @@ def gen_output(args, h, s, fn, zip):
                 cmp = brotli.compress(gj)
             gjfile.write(cmp)
 
+    fc.properties['path'] = ref
     if args.dump_geojson:
         print(gj)
 
@@ -296,12 +341,71 @@ def process(args, f, fn, zip):
     try:
         (h, s) = bufr_decode(f, args)
 
+    except Exception as e:
+        logging.info(f"exception processing {fn} from {zip}: {e}")
+
     except CodesInternalError as err:
         traceback.print_exc(file=sys.stderr)
-    gen_output(args, h, s, fn, zip)
+    else:
+        gen_output(args, h, s, fn, zip)
 
 def update_summary(args, updated_stations):
-    pass
+    if args.summary and os.path.exists(args.summary):
+        with open(args.summary, 'rb') as f:
+            s = f.read().decode()
+            summary = geojson.loads(s)
+            logging.debug(f'read summary from {args.summary}')
+    else:
+        logging.debug(f'no summary file')
+        summary = dict()
+
+    if args.stations and os.path.exists(args.stations):
+        with open(args.stations, 'rb') as f:
+            s = f.read().decode()
+            stations = orjson.loads(s)
+            logging.debug(f'read stations from {args.stations}')
+    else:
+        logging.debug(f'no stations file')
+        stations = dict()
+
+    for id, asc in  updated_stations:
+        if id in summary:
+            # append, sort and de-duplicate
+            oldlist = summary[id]['ascents']
+            oldlist.append(asc)
+            newlist = sorted(oldlist, key=itemgetter('syn_timestamp'), reverse=True)
+            # https://stackoverflow.com/questions/9427163/remove-duplicate-dict-in-list-in-python
+            seen = set()
+            dedup = []
+            for d in newlist:
+                t = d['syn_timestamp']
+                if t not in seen:
+                    seen.add(t)
+                    dedup.append(d)
+            summary[id]['ascents'] = dedup
+        else:
+            if id in stations:
+                st =  stations[id]
+            else:
+                # anonymous + mobile stations
+                st = {
+                    "name": asc['station_id'],
+                    "lat" : asc['lat'],
+                    "lon" : asc['lon'],
+                    "elevation" : asc['elevation']
+                }
+            st['ascents'] = [asc]
+            summary[id] = st
+
+        js = orjson.dumps(summary, option=orjson.OPT_INDENT_2)
+        #pprint(summary)
+
+        fd, path = tempfile.mkstemp()
+        os.write(fd, js)
+        #os.sync(fd)
+        os.close(fd)
+        os.rename(path, args.summary)
+
 
 def main():
     parser = argparse.ArgumentParser(description='decode radiosonde BUFR report',
@@ -317,7 +421,8 @@ def main():
     parser.add_argument('--brotli', action='store_true', default=False)
     parser.add_argument('--by-basename', action='store_true', default=False)
     parser.add_argument('--mkdirs', action='store_true', default=False)
-    parser.add_argument('--update_summary', action='store', default=None)
+    parser.add_argument('--summary', action='store', default=None)
+    parser.add_argument('--stations', action='store', default=None)
     parser.add_argument('files', nargs='*')
 
     args = parser.parse_args()
@@ -325,7 +430,14 @@ def main():
     level = logging.WARNING
     if args.verbose:
         level = logging.DEBUG
+
+    # f = OneLineExceptionFormatter('%(asctime)s|%(levelname)s|%(message)s|', '%m/%d/%Y %I:%M:%S %p')
+    # root = logging.getLogger()
+    # root.setLevel(level)
+    # root.setFormatter(f)
+
     logging.basicConfig(level=level)
+
 
     for f in args.files:
         (fn, ext) = os.path.splitext(os.path.basename(f))
@@ -347,15 +459,15 @@ def main():
                 else:
                     logging.debug(f"processing: {info.filename} from {f}")
                     (bn, ext) = os.path.splitext(info.filename)
-                    process(args, file, bn, fn)
+                    process(args, file, info.filename, f)
                     file.close()
                     os.remove(path)
         else:
             file = open(f, 'rb')
-            process(args, file, fn, "")
+            process(args, file, f, None)
             file.close()
 
-    if args.update_summary:
+    if args.summary:
         update_summary(args, updated_stations)
     logging.debug('Finished')
 
